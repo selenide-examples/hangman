a2552a238a70fb5fe9720849bf64160e679069bd
Andrei Solntsev <drusha49@hotmail.com>
2011-10-25 18:45:15 +0300

    Use project selenide to easier using Selenium

--- src/test/java/ee/era/hangman/uitest/HangmanSpec.java
+++ src/test/java/ee/era/hangman/uitest/HangmanSpec.java
@@ -1,11 +1,23 @@
 package ee.era.hangman.uitest;
 
+import com.github.selenide.UITest;
 import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class HangmanSpec extends UITest {
   @Test
-  public void test1() {
-    System.out.println("TODO"); // TODO implement test
+  public void showsGameControls() {
     open("/game");
+
+    assertTrue(webdriver.findElement(By.id("topic")).isDisplayed());
+    assertTrue(webdriver.findElement(By.id("wordInWork")).isDisplayed());
+    assertTrue(webdriver.findElement(By.id("alphabet")).isDisplayed());
+    assertTrue(webdriver.findElement(By.id("hangmanImageContainer")).isDisplayed());
+
+//    assertThat(webdriver.findElement(By.id("topic")).getText(), equalTo("дом"));
+//    assertThat(webdriver.findElement(By.id("wordInWork")).getText(), equalTo("______"));
   }
 }
