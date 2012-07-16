1e648ca1c07eeb4e032da6049d71533c0d0d818a
Andrei Solntsev <drusha49@hotmail.com>
2012-07-17 01:51:15 +0300

    updated to Selenide 1.4

--- src/test/java/ee/era/hangman/uitest/HangmanSpec.java
+++ src/test/java/ee/era/hangman/uitest/HangmanSpec.java
@@ -1,46 +1,55 @@
 package ee.era.hangman.uitest;
 
-import com.github.selenide.Condition;
-import com.github.selenide.UITest;
+import static com.codeborne.selenide.DOM.*;
+import static com.codeborne.selenide.Navigation.*;
+
+import ee.era.hangman.Launcher;
 import ee.era.hangman.model.Word;
 import ee.era.hangman.model.Words;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import static com.github.selenide.Condition.hasClass;
-import static com.github.selenide.Condition.hasText;
-import static com.github.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.hasClass;
+import static com.codeborne.selenide.Condition.hasText;
+import static com.codeborne.selenide.Condition.visible;
 import static ee.era.hangman.di.DependencyInjection.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-public class HangmanSpec extends UITest {
+public class HangmanSpec {
+  @BeforeClass
+  public static void startServer() throws Exception {
+    wire(Words.class, WordsMock.class);
+    new Launcher(8080).run();
+    baseUrl = "http://localhost:8080/hangman";
+  }
+
   @Before
   public void startGame() {
-    wire(Words.class, WordsMock.class);
     open("/game");
   }
 
   @Test
   public void showsGameControls() {
-    assertTrue(webdriver.findElement(By.id("topic")).isDisplayed());
-    assertTrue(webdriver.findElement(By.id("wordInWork")).isDisplayed());
-    assertTrue(webdriver.findElement(By.id("alphabet")).isDisplayed());
-    assertTrue(webdriver.findElement(By.id("hangmanImageContainer")).isDisplayed());
+    assertTrue(getElement(By.id("topic")).isDisplayed());
+    assertTrue(getElement(By.id("wordInWork")).isDisplayed());
+    assertTrue(getElement(By.id("alphabet")).isDisplayed());
+    assertTrue(getElement(By.id("hangmanImageContainer")).isDisplayed());
 
-    assertThat(webdriver.findElement(By.id("topic")).getText(), equalTo("дом"));
-    assertThat(webdriver.findElement(By.id("wordInWork")).getText(), equalTo("______"));
+    assertThat(getElement(By.id("topic")).getText(), equalTo("дом"));
+    assertThat(getElement(By.id("wordInWork")).getText(), equalTo("______"));
   }
 
   @Test
   public void guessLetterByClickingLetter() {
     getElement(By.xpath("//*[@letter='О']")).click();
-    waitFor(By.xpath("//*[@letter='О']"), hasClass("used"));
+    waitUntil(By.xpath("//*[@letter='О']"), hasClass("used"));
 
     getElement(By.xpath("//*[@letter='Б']")).click();
-    waitFor(By.xpath("//*[@letter='Б']"), hasClass("nonused"));
+    waitUntil(By.xpath("//*[@letter='Б']"), hasClass("nonused"));
   }
 
   @Test
@@ -51,7 +60,7 @@ public class HangmanSpec extends UITest {
     getElement(By.xpath("//*[@letter='Г']")).click();
     getElement(By.xpath("//*[@letter='В']")).click();
     getElement(By.xpath("//*[@letter='Ь']")).click();
-    waitFor(By.id("startGame"), visible);
+    waitFor(By.id("startGame"));
     assertElement(By.id("gameWin"), visible);
     assertElement(By.id("wordInWork"), hasText("гвоздь"));
   }
