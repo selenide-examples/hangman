33f595a0dc832498e8da10bc5b1b77d7be39c617
Andrei Solntsev <drusha49@hotmail.com>
2012-11-30 21:35:40 +0200

    Started Selenide 1.9-SNAPSHOT; using gradle 1.3

---
+++ selenide_1_9/test/ee/uitest/HangmanSpec.java
@@ -0,0 +1,79 @@
+package ee.uitest;
+
+import com.codeborne.selenide.ShouldableWebElement;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.DOM.$;
+import static com.codeborne.selenide.DOM.$$;
+import static com.codeborne.selenide.DOM.waitUntil;
+import static com.codeborne.selenide.Navigation.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static org.junit.Assert.assertEquals;
+
+public class HangmanSpec extends AbstractHangmanTest {
+  @Before
+  public void startGame() {
+    open("/hangman");
+    $(byText("ENG")).click();
+  }
+
+  @Test
+  public void showsTopicAndMaskedWordAtTheBeginning() {
+    $("#topic").shouldHave(text("house"));
+    $("#wordInWork").shouldHave(text("____"));
+  }
+
+  @Test
+  public void userCanGuessLetters() {
+    letter("S").click();
+    $("#wordInWork").shouldHave(text("s___"));
+    letter("S").shouldHave(cssClass("used"));
+  }
+
+  @Test
+  public void userWinsWhenAllLettersAreGuessed() {
+    letter("S").click();
+    letter("O").click();
+    letter("F").click();
+    letter("A").click();
+    $("#gameWin").shouldBe(visible);
+  }
+
+  @Test
+  public void userHasNoMoreThan6Tries() {
+    letter("B").click();
+    letter("D").click();
+    letter("E").click();
+    letter("G").click();
+    letter("H").click();
+    letter("I").click();
+    letter("J").click();
+    letter("B").shouldHave(cssClass("nonused"));
+    $("#gameLost").shouldBe(visible);
+  }
+
+  @Test
+  public void userCanChooseLanguage() {
+    $(By.linkText("EST")).click();
+    waitUntil("#topic", hasText("maja"));
+    $("#wordInWork").shouldHave(text("____"));
+    assertEquals(27, $$("#alphabet .letter").size());
+
+    $(By.linkText("RUS")).click();
+    waitUntil("#topic", hasText("дом"));
+    $("#wordInWork").shouldHave(text("______"));
+    assertEquals(33, $$("#alphabet .letter").size());
+
+    $(By.linkText("ENG")).click();
+    waitUntil("#topic", hasText("house"));
+    $("#wordInWork").shouldHave(text("____"));
+    assertEquals(26, $$("#alphabet .letter").size());
+  }
+
+  private ShouldableWebElement letter(String letter) {
+    return $(byText(letter));
+  }
+}
