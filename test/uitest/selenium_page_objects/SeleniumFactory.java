a83705f35e632a9e9f48860db3b79c3470cb8afe
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-08-22 20:02:14 +0300

    fix compilation error after upgrading Selenide
    WebDriverManager was replaced by SeleniumManager

--- test/uitest/selenium_page_objects/SeleniumFactory.java
+++ test/uitest/selenium_page_objects/SeleniumFactory.java
@@ -1,6 +1,5 @@
 package uitest.selenium_page_objects;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -13,16 +12,15 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 class SeleniumFactory {
   static WebDriver createDriver() {
     String browser = System.getProperty("selenide.browser", "chrome");
-    switch (browser) {
-      case "chrome": return initChromeDriver();
-      case "firefox": return initFirefoxDriver();
-      case "edge": return initEdgeDriver();
-      default: throw new IllegalArgumentException("Unknown selenide.browser='" + browser + "'");
-    }
+    return switch (browser) {
+      case "chrome" -> initChromeDriver();
+      case "firefox" -> initFirefoxDriver();
+      case "edge" -> initEdgeDriver();
+      default -> throw new IllegalArgumentException("Unknown selenide.browser='" + browser + "'");
+    };
   }
 
   private static ChromeDriver initChromeDriver() {
-    WebDriverManager.chromedriver().setup();
     ChromeOptions options = new ChromeOptions();
     setChromiumHeadless(options);
     options.addArguments("--remote-allow-origins=*");
@@ -30,7 +28,6 @@ class SeleniumFactory {
   }
 
   private static FirefoxDriver initFirefoxDriver() {
-    WebDriverManager.firefoxdriver().setup();
     FirefoxOptions options = new FirefoxOptions();
     if ("true".equals(System.getProperty("selenide.headless"))) {
       options.addArguments("-headless");
@@ -39,7 +36,6 @@ class SeleniumFactory {
   }
 
   private static EdgeDriver initEdgeDriver() {
-    WebDriverManager.edgedriver().setup();
     EdgeOptions options = new EdgeOptions();
     setChromiumHeadless(options);
     options.addArguments("--remote-allow-origins=*");
